a25b7f8cc99ddcfcd2d49ad9380f4a459cda00ec
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2020-09-29 18:34:36 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,32 +4,63 @@
 
 def order(values: list = None) -> list:
     if values is None:
-        # TODO: demander les valeurs ici
-        pass
-
-    return []
+        values = []
+        for i in range(0, 10):
+            values.append(input())
+        values.sort()
+    return values
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: demander les mots ici
-        pass
-
-    return False
+        words = [input(), input()]
+        for char in words[0]:
+            if char not in words[1]:
+                return False
+    return True
 
 
 def contains_doubles(items: list) -> bool:
+    for i, item in enumerate(items):
+        for j, words in enumerate(items):
+            if item == words and 1 != j:
+                return True
     return False
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    average = {}
+    for i in student_grades.items():
+        average[i[0]] = get_average(i[1])
+    max = 0
+    max_key = ""
+    for items in average.items():
+        if max < items[1]:
+            max = items[1]
+            max_key = items[0]
+    return {max_key: max}
+
+
+def get_average(l: list):
+    val = 0
+    for i in l:
+        val += int(i)
+    return val / len(l)
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
-    #       Retourner le tableau de lettres
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letters = [sentence.count(alphabet[i] or alphabet[i].upper()) for i in range(0, len(alphabet))]
+    for i in range(0, len(letters)):
+        letters[i] = [letters[i], i]
+    letters.sort()
+    for i in range(0, len(letters)):
+        if letters[len(letters)-1-i][0] > 5:
+            print(f"{alphabet[letters[len(letters)-1-i][1]]} : {letters[len(letters)-1-i][0]}")
+
+    #Retourner le tableau de lettres
 
     return {}
 
@@ -46,10 +77,10 @@ def print_recipe(ingredients) -> None:
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
-    order()
+    ## order()
 
     print(f"On vérifie les anagrammes...")
-    anagrams()
+    ## anagrams()
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
@@ -62,10 +93,12 @@ def main() -> None:
     frequence(sentence)
 
     print("On enregistre les recettes...")
-    recipes = get_recipes()
+    ## recipes = get_recipes()
 
     print("On affiche une recette au choix...")
-    print_recipe(recipes)
+
+
+## print_recipe(recipes)
 
 
 if __name__ == '__main__':
